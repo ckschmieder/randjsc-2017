887875269d0236abe4d2aa22d91bb0788cff89e6
Chris <ckschmieder@gmail.com>
2017-03-10 16:58:19 -0500

    added patricles settings to js

--- RandJSC/components/scripts/script.js
+++ RandJSC/components/scripts/script.js
@@ -21,6 +21,118 @@ jQuery(document).ready(function ($) {
 
 	});
 
+	particlesJS("particles-js", {
+	"particles": {
+		"number": {
+			"value": 289,
+			"density": {
+				"enable": true,
+				"value_area": 1260
+			}
+		},
+		"color": {
+			"value": "#586336"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#000000"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": .9,
+			"random": false,
+			"anim": {
+				"enable": false,
+				"speed": 1,
+				"opacity_min": 0.7,
+				"sync": false
+			}
+		},
+		"size": {
+			"value": 9,
+			"random": true,
+			"anim": {
+				"enable": false,
+				"speed": 40,
+				"size_min": 7,
+				"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"distance": 125,
+			"color": "#586336",
+			"opacity": 0.7,
+			"opacity_min": 0.5,
+			"width": 2.8
+		},
+		"move": {
+			"enable": true,
+			"speed": 0.35,
+			"direction": "none",
+			"random": false,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "grab"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 135,
+				"line_linked": {
+					"opacity": .6
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 25,
+				"duration": 2.2
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
+});
+
 
 
 });
@@ -114,7 +226,7 @@ jQuery(window).on('load', function (e) {
  }
 
  // use setTimeout() to execute
- setTimeout(bannerdelay, 6500)
+ setTimeout(bannerdelay, 4000)
 
 
 /* jQuery('body').on('load', function(){
